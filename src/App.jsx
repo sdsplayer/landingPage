d61524bdd1fdf3a560c6af0abed93365107de798
AxrorjonA <adhamjonovaxrorjon@gmail.com>
2023-04-15 15:31:25 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Footer from './components/Footer/index'
+function App() {
+    return (
+        <div>
+            <Footer/>
+        </div>
+    )
+}
+export default App
